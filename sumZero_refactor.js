5d25d4b3832987ffc39a9fc93ad76e8482d1030e
Joohoon Kim <jayjhkim7@gmail.com>
2021-08-28 11:10:29 -0400

    sumZero using pointer

---
+++ sumZero_refactor.js
@@ -0,0 +1,24 @@
+console.log(sumZero_refactor([-3, -2, -1, 0, 1, 2, 3])); //[-3, 3]
+console.log(sumZero_refactor([-2, 0, 1, 3]));
+console.log(sumZero_refactor([1, 2, 3]));
+console.log(sumZero_refactor([-8, -7, -5, -2, 0, 1, 2, 3, 4, 5, 6]));
+
+function sumZero_refactor(arr, p1 = 0, p2 = arr.length - 1) {
+  let a = arr[p1];
+  let b = arr[p2];
+  debugger;
+  if (a > 0 || b < 0) {
+    return undefined;
+  } else if (p1 === p2) {
+    return undefined;
+  } else if (a + b === 0) {
+    return [a, b];
+  } else if (Math.abs(a) < Math.abs(b)) {
+    p2--;
+  } else if (Math.abs(a) > Math.abs(b)) {
+    p1++;
+  }
+  return sumZero_refactor(arr, p1, p2);
+}
+
+sumZero_refactor([-8, -7, -5, -2, 0, 1, 2, 3, 4, 5, 6]);
