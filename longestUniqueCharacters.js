40a88e2249d4a717bd7572fbb7b75a27e0b99573
Joohoon Kim <jayjhkim7@gmail.com>
2021-08-31 08:45:08 -0400

    luc wip

---
+++ longestUniqueCharacters.js
@@ -0,0 +1,21 @@
+function luc(word, prevCount = 0, k = word.length - 1) {
+  let count = 0;
+  arr = [];
+  for (let i = word.length - 1; i >= 0; i--) {
+    if (!arr.includes(word[i])) {
+      arr.push(word[i]);
+      count++;
+    } else {
+      k--;
+    }
+  }
+
+  let curCount = count > prevCount ? count : prevCount;
+  if (k <= 0) {
+    return word.slice(-1 * curCount, -1);
+  }
+  console.log(curCount, k);
+  return luc(word, curCount, k);
+}
+
+console.log(luc('hellothere'));
